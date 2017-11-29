a46219ae314e3ccafe227ebc9a21a451f8c77c2e
GODSKADI <agarcia-fraileperez@iesesteveterradas.cat>
2017-11-29 17:03:45 +0100

    responder

--- responder.php
+++ responder.php
@@ -1,25 +1,59 @@
-<?php include 'connection.php'; ?>
-<table>
-<?php
-$query =$pdo->prepare("SELECT * FROM Consulta");
-$query->execute(); 
-$row=$query->fetch();
-echo
-"<tr>"
-			."<td> ID </td>"
-			."<td> Pregunta </td>"
-			."</tr>";
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="icon" type="image/ico" href="img/logo.ico" />
+	<link href="style.css" type="text/css" rel="stylesheet">
+	<script type="text/javascript" src="funciones.js"></script>
+	<title>Votaciones</title>
+</head>
 
-while($row){
-	echo 
-			"<tr>"
-			."<td>" . $row['ID']."</td>"
-			."<td>" . $row['Desc_Pregunta']."</td>"
+<body id="snow">
 
-			."</tr>";
-
+	<header>
+		<img id="banner" src="img\banner.png" width="1280px" height="321px">
+		<div>
+			<nav>
+				<ul class="menu">
+					<li><a href="inicio.php">INICIO</a></li>
+					<li><a>CONSULTAS</a>
+					<ul>
+					<li><a href="formulario.php">CREAR</a>
+					<a href="responder.php">RESPONDER</a></li>
+					</ul>
+					</li>
+				</ul>
+			</nav>
+		</div>
+		<div class="vota">
+			<h1>Cuestiones</h1>
+		</div>
+	</header>
+	<?php include 'connection.php'; ?>
+	<table>
+	<?php
+	$query =$pdo->prepare("SELECT * FROM Consulta");
+	$query->execute(); 
 	$row=$query->fetch();
-}
+	echo
+	"<tr>"
+				."<td> ID </td>"
+				."<td> Pregunta </td>"
+				."</tr>";
 
-?>	
-</table>
+	while($row){
+		echo 
+				"<tr>"
+				."<td>" . $row['ID']."</td>"
+				."<td>" . $row['Desc_Pregunta']."</td>"
+
+				."</tr>";
+
+		$row=$query->fetch();
+	}
+
+	?>	
+	</table>
+	<footer>Votaciones Jonatan y Adria</footer>
+
+</body>
+</html>
